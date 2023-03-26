957b3bd67b514167fe8781e05725f200134c2fa8
F1F7Y <64418963+F1F7Y@users.noreply.github.com>
2023-03-26 14:38:22 +0200

    Add examples

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -5,7 +5,32 @@
 
 void Plugin::Main() {}
 void Plugin::DLLLoadEngine() {
+//-----------------------------------------------------------------------------
+// Custom ConCommand example
+//-----------------------------------------------------------------------------
 	RegisterConCommand("cplug_test", [](const CCommand& command){ spdlog::info("Gaming"); }, "", 0);
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Custom ConVar example
+//-----------------------------------------------------------------------------
+	ConVar *pCustomConVar = new ConVar( "CPLUG_CONVAR", "Very cool", FCVAR_CLIENTDLL, "Example convar" );
+//-----------------------------------------------------------------------------
 }
+
 void Plugin::DLLLoadClient() {}
 void Plugin::DLLLoadServer() {}
+
+
+//-----------------------------------------------------------------------------
+// Custom squirrel function example
+//-----------------------------------------------------------------------------
+#include "squirrel.h"
+#include "squirrelautobind.h"
+
+ADD_SQFUNC("void", CPlugTest, "", "", ScriptContext::CLIENT | ScriptContext::SERVER | ScriptContext::UI)
+{
+	spdlog::info( "Hi :)" );
+	return SQRESULT_NULL;
+}
+//-----------------------------------------------------------------------------
