878bd8b2892df52c4bbab1b01ab8804903a5224e
okvdai <108829133+okvdai@users.noreply.github.com>
2023-11-14 22:12:36 +0100

    Use plugin handle
    Spending half of a day to write 2 lines was quite fun (kill me)

--- src/lib/loader.cpp
+++ src/lib/loader.cpp
@@ -12,11 +12,11 @@
 
 void PLUGIN_INIT(PluginInitFuncs* funcs, PluginNorthstarData* data) {
 	g_pPlugin = new Plugin;
+	g_pPlugin->handle = data->pluginHandle;
 	g_pPlugin->logger = (logger_t)funcs->logger;
 	spdlog::default_logger()->sinks().pop_back();
 	spdlog::default_logger()->sinks().push_back(std::make_shared<my_sink>());
 	spdlog::info("CPluginTest succesfully initialised!");
-
 	g_pPlugin->Init(funcs, data);
 	g_pPlugin->Main();
 }
